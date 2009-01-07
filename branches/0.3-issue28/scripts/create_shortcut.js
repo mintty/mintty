dd18cbc0ec80ea2cd68f83b70fc76ae615b0879a
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-01-07 07:37:51 +0000

    Created branch off 0.3 for addressing issue 28.

---
+++ branches/0.3-issue28/scripts/create_shortcut.js
@@ -0,0 +1,23 @@
+ws = WScript;
+sh = ws.CreateObject("WScript.Shell");
+try {
+  cygbase = sh.RegRead(
+    "HKLM\\Software\\Cygnus Solutions\\Cygwin\\mounts v2\\/\\native"
+  );
+} catch (e) {
+  ws.Echo("Error: Could not read Cygwin registry key.");
+  ws.Quit(1);
+}
+cwd = sh.CurrentDirectory;
+link = sh.CreateShortcut("MinTTY.lnk");
+link.TargetPath = cwd + "\\mintty.exe";
+link.Arguments = "/bin/bash -l";
+link.WorkingDirectory = cygbase + "\\bin";
+link.IconLocation = cwd + "\\mintty.exe,0";
+link.Description = "Cygwin Terminal";
+try {
+  link.Save();
+} catch (e) {
+  ws.Echo("Error: Could not write shortcut to disk.");
+  ws.Quit(1);
+}
