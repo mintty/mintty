ae764ed5ab6b0955a5cd589c55899e780acb2c26
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-04-24 21:40:14 +0000

    Took fix for issue 91 (initial window size bigger than screen) to 0.3 branch from trunk:242.

--- branches/0.3/winmain.c
+++ branches/0.3/winmain.c
@@ -889,24 +889,24 @@ main(int argc, char *argv[])
     RegisterClass(&wndclass);
   }
 
+ /* Create initial window.
+  * Its real size has to be set after loading the fonts and determining their
+  * size, but the window has to exist to do that.
+  */
+  wnd = CreateWindow(APPNAME, APPNAME,
+                     WS_OVERLAPPEDWINDOW | (cfg.scrollbar ? WS_VSCROLL : 0),
+                     x, y, 300, 200, null, null, inst, null);
+
  /*
-  * Guess some defaults for the window size. This all gets
-  * updated later, so we don't really care too much. However, we
-  * do want the font width/height guesses to correspond to a
-  * large font rather than a small one...
+  * Determine extra_{width,height}.
   */
   {
-    int guess_width = 25 + 20 * cols;
-    int guess_height = 28 + 20 * rows;
-    RECT r;
-    get_fullscreen_rect(&r);
-    if (guess_width > r.right - r.left)
-      guess_width = r.right - r.left;
-    if (guess_height > r.bottom - r.top)
-      guess_height = r.bottom - r.top;
-    uint style = WS_OVERLAPPEDWINDOW | (cfg.scrollbar ? WS_VSCROLL : 0);
-    wnd = CreateWindow(APPNAME, APPNAME, style, x, y,
-                        guess_width, guess_height, null, null, inst, null);
+    RECT cr, wr;
+    GetWindowRect(wnd, &wr);
+    GetClientRect(wnd, &cr);
+    offset_width = offset_height = 0;
+    extra_width = wr.right - wr.left - cr.right + cr.left;
+    extra_height = wr.bottom - wr.top - cr.bottom + cr.top;
   }
 
   win_init_menus();
@@ -927,18 +927,6 @@ main(int argc, char *argv[])
   */
   win_init_fonts();
   win_init_palette();
-
- /*
-  * Correct the guesses for extra_{width,height}.
-  */
-  {
-    RECT cr, wr;
-    GetWindowRect(wnd, &wr);
-    GetClientRect(wnd, &cr);
-    offset_width = offset_height = 0;
-    extra_width = wr.right - wr.left - cr.right + cr.left;
-    extra_height = wr.bottom - wr.top - cr.bottom + cr.top;
-  }
   
  /*
   * Set up a caret bitmap, with no content.
