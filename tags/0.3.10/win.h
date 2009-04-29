41a908590591ee169b0a74f24c3fd2511e428842
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-04-29 21:23:21 +0000

    Tagged release 0.3.10.

---
+++ tags/0.3.10/win.h
@@ -0,0 +1,36 @@
+#ifndef WIN_H
+#define WIN_H
+
+void win_update(void);
+void win_schedule_update(void);
+
+bool win_ambig_cjk_wide(void);
+int  win_char_width(int uc);
+void win_text(int, int, wchar *, int, uint, int);
+void win_cursor(int, int, wchar *, int, uint, int);
+void win_sys_cursor(int x, int y);
+void win_update_mouse(void);
+void win_capture_mouse(void);
+void win_bell(int);
+
+void win_set_title(char *);
+void win_set_sbar(int, int, int);
+void win_set_palette(uint n, uint8 r, uint8 g, uint8 b);
+void win_reset_palette(void);
+
+void win_move(int x, int y);
+void win_resize(int rows, int cols);
+void win_set_zoom(bool);
+void win_set_zorder(bool top);
+void win_set_iconic(bool);
+bool win_is_iconic(void);
+void win_get_pos(int *x, int *y);
+void win_get_pixels(int *x, int *y);
+void win_popup_menu(void);
+
+void win_copy(wchar *, int *attrs, int len);
+void win_paste(void);
+
+void win_set_timer(void_fn cb, uint ticks);
+
+#endif
