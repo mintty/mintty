6324a2422434fb34bcde0f58e0e20ffd64d48517
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-05-03 05:39:01 +0000

    Added license message to --version output. Also added "no warranty" message.

--- appinfo.h
+++ appinfo.h
@@ -7,12 +7,15 @@
 #define APPNAME "MinTTY"
 #define APPVER stringify(VERSION)
 #define APPDESC "Cygwin terminal"
-#define COPYRIGHT "Copyright (C) 2008-09 Andy Koppe"
 #define WEBSITE "http://mintty.googlecode.com"
+#define COPYRIGHT "Copyright (C) 2008-09 Andy Koppe"
+
+#define LICENSE \
+"Licensed under GPL version 3 or later.\n" \
+"There is no warranty, to the extent permitted by law." \
 
 #define APPINFO \
-  "This software is released under the terms of the\n" \
-  "GNU General Public License v3 or later.\n" \
+  LICENSE "\n" \
   "\n" \
   "Thanks to Simon Tatham and the other contributors for their\n"\
   "great work on PuTTY, which MinTTY is largely based on.\n" \
