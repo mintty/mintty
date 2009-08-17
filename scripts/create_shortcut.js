92264828d1adb8027f07a8e6d956b912b16f8b3c
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-08-17 19:20:18 +0000

    Various tweaks.

--- scripts/create_shortcut.js
+++ scripts/create_shortcut.js
@@ -1,22 +1,18 @@
 ws = WScript;
 sh = ws.CreateObject("WScript.Shell");
 
-cygkey15 = "Cygnus Solutions\\Cygwin\\mounts v2\\/\\native"
-cygkey17 = "Cygwin\\setup\\rootdir";
+cygkey = "Cygwin\\setup\\rootdir";
 softkey = "HKLM\\Software\\";
 softkey64 = softkey + "WoW6432Node\\";
 
-try { cygbase = sh.RegRead(softkey + cygkey17); }
+try { cygbase = sh.RegRead(softkey + cygkey); }
 catch (e) {
-  try { cygbase = sh.RegRead(softkey64 + cygkey17); }
+  try { cygbase = sh.RegRead(softkey64 + cygkey); }
   catch (e) {
-    try { cygbase = sh.RegRead(softkey + cygkey15); }
-    catch (e) {
-      try { cygbase = sh.RegRead(softkey64 + cygkey15); }
-      catch (e) {
-        ws.Echo("Error: Could not find Cygwin registry key.");
-        ws.Quit(1); 
-} } } }
+    ws.Echo("Error: Could not find Cygwin 1.7 registry key.");
+    ws.Quit(1); 
+  }
+}
 
 cwd = sh.CurrentDirectory;
 link = sh.CreateShortcut("MinTTY.lnk");
