9c2277e3564b034491818be4477118410634a3c0
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-09-06 09:09:15 +0000

    Added separate Makefile targets for cygwin15 and cygwin17 zips, with slightly different create_shortcut scripts due to the different Cygwin registry key locations.

---
+++ scripts/create_shortcut-cygwin15.js
@@ -0,0 +1,29 @@
+ws = WScript;
+sh = ws.CreateObject("WScript.Shell");
+
+cygkey = "Cygnus Solutions\\Cygwin\\mounts v2\\/\\native"
+softkey = "HKLM\\Software\\";
+softkey64 = softkey + "WoW6432Node\\";
+
+try { cygbase = sh.RegRead(softkey + cygkey); }
+catch (e) {
+  try { cygbase = sh.RegRead(softkey64 + cygkey); }
+  catch (e) {
+    ws.Echo("Error: Could not find Cygwin 1.7 registry key.");
+    ws.Quit(1); 
+  }
+}
+
+cwd = sh.CurrentDirectory;
+link = sh.CreateShortcut("MinTTY.lnk");
+link.TargetPath = cwd + "\\mintty.exe";
+link.Arguments = "-";
+link.WorkingDirectory = cygbase + "\\bin";
+link.IconLocation = cwd + "\\mintty.exe,0";
+link.Description = "Cygwin Terminal";
+
+try { link.Save(); }
+catch (e) {
+  ws.Echo("Error: Could not write shortcut to disk.");
+  ws.Quit(1);
+}
