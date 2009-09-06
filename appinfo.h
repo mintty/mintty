3b9bc95c968d6dc06d09a36d045b95b5260abfea
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-09-06 09:06:58 +0000

    Version bump to 0.5-b2.

--- appinfo.h
+++ appinfo.h
@@ -12,11 +12,11 @@
 #define AUTHOR  "Andy Koppe"
 #define YEARS   "2008-09"
 
-#define VERSION "0.5-b1"
+#define VERSION "0.5-b2"
 #define MAJOR_VERSION  0
 #define MINOR_VERSION  5
 #define PATCH_NUMBER   0
-#define BUILD_NUMBER 2
+#define BUILD_NUMBER   3
 
 #if defined BRANCH && defined REVISION
 #undef VERSION
