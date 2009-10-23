9060dcd6e09942e1ed457d19d9480c13c779820a
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-10-23 16:44:35 +0000

    Version bump to 0.5.2 and manual page update. Added shortcuts overview and Ctrl+click feature to usage section.

--- branches/0.5/appinfo.h
+++ branches/0.5/appinfo.h
@@ -12,10 +12,10 @@
 #define AUTHOR  "Andy Koppe"
 #define YEARS   "2008-09"
 
-#define VERSION "0.5.1"
+#define VERSION "0.5.2"
 #define MAJOR_VERSION  0
 #define MINOR_VERSION  5
-#define PATCH_NUMBER   1
+#define PATCH_NUMBER   2
 #define BUILD_NUMBER   1
 
 #if defined BRANCH && defined REVISION
