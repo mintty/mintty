e7b0a84469698f2478d1f10d6ac68b67ac1f69e4
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2009-11-07 06:48:41 +0000

    Bumped cygport version to 0.5.3-1.

--- branches/0.5/appinfo.h
+++ branches/0.5/appinfo.h
@@ -16,7 +16,7 @@
 #define MAJOR_VERSION  0
 #define MINOR_VERSION  5
 #define PATCH_NUMBER   3
-#define BUILD_NUMBER   0
+#define BUILD_NUMBER   1
 
 #if defined BRANCH && defined REVISION
 #undef VERSION
