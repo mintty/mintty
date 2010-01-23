61e7010e01f5907f82695ae5cef37a985a68a579
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2010-01-23 09:20:41 +0000

    Use the -AP/-P options to mkshortcut instead of passing a path obtained with cygpath, because mkshortcut does not yet deal with non-ASCII characters on the command line correctly.

--- branches/0.5/cygport/postinstall.sh
+++ branches/0.5/cygport/postinstall.sh
@@ -1,7 +1,9 @@
 PROGS=$(/bin/cygpath -AP)
-if [ ! -w "$PROGS" ]; then
-  PROGS=$(/bin/cygpath -P)
+if [ -w "$PROGS" ]; then
+  /bin/mkdir -p "$PROGS/Cygwin"
+  /bin/mkshortcut -AP -n Cygwin/mintty -a - -d Terminal /bin/mintty.exe
+  /bin/chmod a+r "$PROGS/Cygwin/mintty.lnk"
+else
+  /bin/mkdir -p "$(/bin/cygpath -P)/Cygwin"
+  /bin/mkshortcut -P -n Cygwin/mintty -a - -d Terminal /bin/mintty.exe
 fi
-/bin/mkdir -p "$PROGS/Cygwin"
-/bin/mkshortcut -n "$PROGS/Cygwin/mintty" -a - -d Terminal /bin/mintty.exe
-/bin/chmod go+r "$PROGS/Cygwin/mintty.lnk"
