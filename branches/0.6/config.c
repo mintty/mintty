2eaf5c0176491c5082285ea393acd0ce66573e06
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2010-04-03 18:34:40 +0000

    Make hold-on-error the default.

--- branches/0.6/config.c
+++ branches/0.6/config.c
@@ -14,7 +14,7 @@
 
 const char *log_file = 0;
 bool utmp_enabled = false;
-hold_t hold = HOLD_NEVER;
+hold_t hold = HOLD_ERROR;
 
 #if CYGWIN_VERSION_API_MINOR >= 222
 static wchar *rc_filename = 0;
