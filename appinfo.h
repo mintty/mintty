c481d1599fc57e6180e1b6c9e334c9f03409910d
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2010-06-02 18:35:26 +0000

    Version bump to 0.7-beta3.

--- appinfo.h
+++ appinfo.h
@@ -12,11 +12,11 @@
 #define AUTHOR  "Andy Koppe"
 #define YEAR    "2010"
 
-#define VERSION "0.7-beta2"
+#define VERSION "0.7-beta3"
 #define MAJOR_VERSION  0
 #define MINOR_VERSION  7
 #define PATCH_NUMBER   0
-#define BUILD_NUMBER   2
+#define BUILD_NUMBER   3
 
 #if defined BRANCH && defined REVISION
 #undef VERSION
