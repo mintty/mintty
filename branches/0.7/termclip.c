5a12784fcf03cabc6664c502367836086b5e8f06
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2010-06-12 21:18:40 +0000

    Don't crash when hitting a key during a paste. Broken since r861.

--- branches/0.7/termclip.c
+++ branches/0.7/termclip.c
@@ -195,18 +195,15 @@ term_cancel_paste(void)
 void
 term_send_paste(void)
 {
-  if (term.paste_len == 0)
-    return;
-
-  if (term.paste_pos < term.paste_len) {
+  if (term.paste_buffer) {
     int i = term.paste_pos;
     while (i < term.paste_len && term.paste_buffer[i++] != '\r');
     child_sendw(term.paste_buffer + term.paste_pos, i - term.paste_pos);
-    term.paste_pos = i;
+    if (i < term.paste_len)
+      term.paste_pos = i;
+    else
+      term_cancel_paste();
   }
-
-  if (term.paste_pos >= term.paste_len)
-    term_cancel_paste();
 }
 
 void
