93eff559684a988b748fa9bd29179dac491e222f
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2010-09-18 04:47:37 +0000

    Merge cygport changes from 0.8 branch. Prepare 0.9b1-1 test release. Correct file locations in README. Put setup.exe's new CYGWINFORALL variable for signifying an 'All Users' installation to use in postinstall and preremove scripts.

--- trunk/cygport/preremove.sh
+++ trunk/cygport/preremove.sh
@@ -1,8 +1,3 @@
-{ PROGS=$(/bin/cygpath -AP)
-  rm --interactive=never "$PROGS/Cygwin/mintty.lnk" &&
-  rmdir --ignore-fail-on-non-empty "$PROGS/Cygwin"
-} ||
-{ PROGS=$(/bin/cygpath -P)
-  rm --interactive=never "$PROGS/Cygwin/mintty.lnk" &&
-  rmdir --ignore-fail-on-non-empty "$PROGS/Cygwin"
-}
+PROGS=$(/bin/cygpath -P $CYGWINFORALL)
+rm --interactive=never "$PROGS/Cygwin/mintty.lnk" &&
+rmdir --ignore-fail-on-non-empty "$PROGS/Cygwin"
