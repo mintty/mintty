ea39c8a21e2dd49dc1f0fbb4afc96da624651782
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2011-04-17 09:19:47 +0000

    Update copyright notices.

--- child.c
+++ child.c
@@ -1,5 +1,5 @@
 // child.c (part of mintty)
-// Copyright 2008-10 Andy Koppe
+// Copyright 2008-11 Andy Koppe
 // Licensed under the terms of the GNU General Public License v3 or later.
 
 #include "child.h"
