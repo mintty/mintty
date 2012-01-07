1aad200e2e385ee81b7e8207d5b4102990b3e68b
andy.koppe <andy.koppe@2782a51e-c42b-11dd-927d-974589228060>
2012-01-07 13:57:06 +0000

    Merge /branches/1.0:r1243-1251 to trunk.

--- winmain.c
+++ winmain.c
@@ -694,18 +694,30 @@ show_msg(FILE *stream, string msg)
     MessageBox(0, msg, APPNAME, MB_OK);
 }
 
-static no_return __attribute__((format(printf, 2, 3)))
-error(bool syntax, char *format, ...) 
+static no_return __attribute__((format(printf, 1, 2)))
+error(char *format, ...) 
 {
   char *msg;
   va_list va;
   va_start(va, format);
   vasprintf(&msg, format, va);
   va_end(va);
-  msg = asform("%s: %s\n%s", main_argv[0], msg,
-               syntax ? "Try '--help' for more information.\n" : "");
+  msg = asform("%s: %s\nTry '--help' for more information.\n",
+               main_argv[0], msg);
   show_msg(stderr, msg);
   exit(1);
+}
+
+static void __attribute__((format(printf, 1, 2)))
+warn(char *format, ...) 
+{
+  char *msg;
+  va_list va;
+  va_start(va, format);
+  vasprintf(&msg, format, va);
+  va_end(va);
+  msg = asform("%s: %s\n", main_argv[0], msg);
+  show_msg(stderr, msg);
 }
 
 int
@@ -752,10 +764,10 @@ main(int argc, char *argv[])
       when 'o': parse_arg_option(optarg);
       when 'p':
         if (sscanf(optarg, "%i,%i%1s", &cfg.x, &cfg.y, (char[2]){}) != 2)
-          error(true, "syntax error in position argument '%s'", optarg);
+          error("syntax error in position argument '%s'", optarg);
       when 's':
         if (sscanf(optarg, "%u,%u%1s", &cfg.cols, &cfg.rows, (char[2]){}) != 2)
-          error(true, "syntax error in size argument '%s'", optarg);
+          error("syntax error in size argument '%s'", optarg);
         remember_arg("Columns");
         remember_arg("Rows");
       when 't': set_arg_option("Title", optarg);
@@ -769,9 +781,9 @@ main(int argc, char *argv[])
         show_msg(stdout, VERSION_TEXT);
         return 0;
       when '?':
-        error(true, "unknown option '%s'", optopt ? shortopt : longopt);
+        error("unknown option '%s'", optopt ? shortopt : longopt);
       when ':':
-        error(true, "option '%s' requires an argument",
+        error("option '%s' requires an argument",
               longopt[1] == '-' ? longopt : shortopt);
     }
   }
@@ -805,10 +817,11 @@ main(int argc, char *argv[])
   }
   
   // Load icon if specified.
-  HICON small_icon = 0, large_icon = 0;
+  HICON large_icon = 0, small_icon = 0;
   if (*cfg.icon) {
+    string icon_file = strdup(cfg.icon);
     uint icon_index = 0;
-    char *comma = strrchr(cfg.icon, ',');
+    char *comma = strrchr(icon_file, ',');
     if (comma) {
       char *start = comma + 1, *end;
       icon_index = strtoul(start, &end, 0);
@@ -817,19 +830,33 @@ main(int argc, char *argv[])
       else
         icon_index = 0;
     }
+    SetLastError(0);
 #if CYGWIN_VERSION_API_MINOR >= 181
-    wchar *win_icon_file = cygwin_create_path(CCP_POSIX_TO_WIN_W, cfg.icon);
-    if (!win_icon_file)
-      error(false, "invalid icon file path '%s'", cfg.icon);
-    ExtractIconExW(win_icon_file, icon_index, &large_icon, &small_icon, 1);
-    free(win_icon_file);
+    wchar *win_icon_file = cygwin_create_path(CCP_POSIX_TO_WIN_W, icon_file);
+    if (win_icon_file) {
+      ExtractIconExW(win_icon_file, icon_index, &large_icon, &small_icon, 1);
+      free(win_icon_file);
+    }
 #else
     char win_icon_file[MAX_PATH];
-    cygwin_conv_to_win32_path(cfg.icon, win_icon_file);
+    cygwin_conv_to_win32_path(icon_file, win_icon_file);
     ExtractIconExA(win_icon_file, icon_index, &large_icon, &small_icon, 1);
 #endif
-    if (!small_icon || !large_icon)
-      error(false, "could not load icon from '%s'", cfg.icon);
+    if (!large_icon) {
+      small_icon = 0;
+      uint error = GetLastError();
+      if (error) {
+        char msg[1024];
+        FormatMessage(
+          FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_MAX_WIDTH_MASK,
+          0, error, 0, msg, sizeof msg, 0
+        );
+        warn("could not load icon from '%s': %s", cfg.icon, msg);
+      }
+      else
+        warn("could not load icon from '%s'", cfg.icon);
+    }
+    delete(icon_file);
   }
 
   inst = GetModuleHandle(NULL);
