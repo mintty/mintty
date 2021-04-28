7b7757886c0ae91ce4e124c23a548e1ad5b99735
mintty <mintty@users.noreply.github.com>
2021-04-29 00:00:00 +0200

    fix termination of synchronous update (#1098)

--- src/termout.c
+++ src/termout.c
@@ -2216,6 +2216,10 @@ set_modes(bool state)
             term.curs.bidimode |= LATTR_BIDISEL;
         when 2026:
           term.suspend_update = state ? 150 : 0;
+          if (!state) {
+            do_update();
+            usleep(1000);  // flush update
+          }
       }
     }
     else { /* SM/RM: set/reset mode */
