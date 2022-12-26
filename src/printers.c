a2801b10db273b51014b2692979a558891180691
Biswapriyo Nath <nathbappai@gmail.com>
2022-12-26 19:20:35 +0530

    Add single macro to log info and errors

--- src/printers.c
+++ src/printers.c
@@ -4,8 +4,13 @@
 #include <wingdi.h>
 #include <winspool.h>
 
-#define dont_debug_printer
-
+#if defined(DEBUG) || defined(_DEBUG) || !defined(NDEBUG)
+  #define LOG_ERROR(str, ...) { fprintf(stderr, "ERROR: %s:%u: " str "\n",__func__, __LINE__, ##__VA_ARGS__); }
+  #define LOG_INFO(str, ...) { fprintf(stdout, "INFO: %s:%u: " str "\n", __func__, __LINE__, ##__VA_ARGS__); }
+#else
+  #define LOG_ERROR(str, ...)
+  #define LOG_INFO(str, ...)
+#endif
 
 #define PRINFTYPE   4
 static PRINTER_INFO_4W * printer_info = null;
@@ -18,12 +23,12 @@ printer_start_enum(void)
   ok = EnumPrintersW(PRINTER_ENUM_LOCAL | PRINTER_ENUM_CONNECTIONS,
                      0, PRINFTYPE, (LPBYTE)printer_info, size, &size, &num);
   int err = GetLastError();
-  //printf("printers ok %d size %d num %d err %d\n", ok, size, num, err);
+  LOG_INFO("printers ok %d size %d num %d err %d", ok, size, num, err);
   if (!ok && err == ERROR_INSUFFICIENT_BUFFER) {
     printer_info = _realloc(printer_info, size);
     ok = EnumPrintersW(PRINTER_ENUM_LOCAL | PRINTER_ENUM_CONNECTIONS,
                        0, PRINFTYPE, (LPBYTE)printer_info, size, &size, &num);
-    //printf("printers ok %d size %d num %d\n", ok, size, num);
+    LOG_INFO("printers ok %d size %d num %d", ok, size, num);
   }
   return ok ? num : 0;
 }
@@ -31,9 +36,7 @@ printer_start_enum(void)
 wstring
 printer_get_name(uint i)
 {
-#ifdef debug_printer
-  printf("Printer %d: %ls\n", i, printer_info[i].pPrinterName);
-#endif
+  LOG_INFO("Printer %d: %ls", i, printer_info[i].pPrinterName);
 #if PRINFTYPE == 1
   return printer_info[i].pName;
 #else
@@ -55,9 +58,7 @@ printer_get_default(void)
 void
 printer_finish_enum(void)
 {
-#ifdef debug_printer
-  printf("Default  : %ls\n", printer_get_default());
-#endif
+  LOG_INFO("Default  : %ls", printer_get_default());
   free(printer_info);
   printer_info = 0;
 }
