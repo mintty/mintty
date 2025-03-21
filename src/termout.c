c5d045be7b2933815adcaf9b07768caba724f2ab
Thomas Wolff <mintty@users.noreply.github.com>
2025-03-21 00:00:00 +0000

    tweak handling of image size limit

--- src/termout.c
+++ src/termout.c
@@ -22,7 +22,7 @@
 
 #define TERM_CMD_BUF_INC_STEP 128
 //#define TERM_CMD_BUF_MAX_SIZE (1024 * 1024)
-#define TERM_CMD_BUF_MAX_SIZE (uint)(1024 + cfg.max_image_size)
+#define TERM_CMD_BUF_MAX_SIZE max(2222, (uint)cfg.max_image_size)
 
 #define SUB_PARS (1 << (sizeof(*term.csi_argv) * 8 - 1))
 
